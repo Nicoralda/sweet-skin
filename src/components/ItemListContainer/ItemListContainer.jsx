0b2da0e6657d6f17d4564f6023add8ede353bb89
Nicole Blanco <nicoletta.4002@gmail.com>
2025-01-26 13:37:03 -0400

    itemListContainer, ItemList, Item y asyncProducts

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,7 +1,24 @@
+import { useState, useEffect } from "react"
+import { getProducts } from "../../asyncProducts"
+import ItemList from "../ItemList/ItemList"
+
 const ItemListContainer = ({ greeting }) => {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        getProducts()
+        .then(response => {
+            setProducts(response)
+        })
+        .catch(error => {
+            console.error(error)
+        })
+    }, [])
+
     return (
         <div>
             <h3>{greeting}</h3>
+            <ItemList products={products}/>
         </div>
     )
 }
