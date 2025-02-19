d51cfe35e2363c784aa0492fff5efd988436625f
Nicole Blanco <nicoletta.4002@gmail.com>
2025-02-18 21:33:38 -0400

    avanzando proyecto final, video clase 9

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBiPU7Ia0t3Usvm3blvEsMXBT47dV5iy5A",
+    authDomain: "sweetskin-ecommerce.firebaseapp.com",
+    projectId: "sweetskin-ecommerce",
+    storageBucket: "sweetskin-ecommerce.firebasestorage.app",
+    messagingSenderId: "99995940819",
+    appId: "1:99995940819:web:4cf009ef51c2aebd242126"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
